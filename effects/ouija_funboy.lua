5d25ee45ed9d5589597891a9dcb7ddbc751d82e1
Connor Mills <connor@virtualized.dev>
2026-05-01 19:45:33 -0600

    Changed ouija to mp ouija

--- effects/ouija_funboy.lua
+++ effects/ouija_funboy.lua
@@ -12,15 +12,15 @@ local create_card_ref = create_card
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
 	local unbanned = false
 	if WOF.flags.ouija_funboy and _type == "Spectral" and not forced_key then
-		forced_key = "c_ouija"
-		if G.GAME.banned_keys["c_ouija"] then
-			G.GAME.banned_keys["c_ouija"] = nil
+		forced_key = "c_mp_ouija_standard"
+		if G.GAME.banned_keys["c_mp_ouija_standard"] then
+			G.GAME.banned_keys["c_mp_ouija_standard"] = nil
 			unbanned = true
 		end
 	end
 	local card = create_card_ref(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
 	if unbanned then
-		G.GAME.banned_keys["c_ouija"] = true
+		G.GAME.banned_keys["c_mp_ouija_standard"] = true
 	end
 	return card
 end
